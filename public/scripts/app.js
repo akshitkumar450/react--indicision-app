447f5f4ac4dfcf83bbda486544d1a713e26a6c7f
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-02 22:24:13 +0530

    conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,83 @@
 
 // JSX
 
+var data = {
+    title: 'indicision app',
+    subtitle: 'put your life',
+    options: ['one', 'two']
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'this is JSX from app.js saying hello'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        data.title
+    ),
+    data.subtitle && data.subtitle.length > 0 && React.createElement(
+        'p',
+        null,
+        data.subtitle
+    ),
+    data.options && data.options.length > 0 ? React.createElement(
+        'p',
+        null,
+        'here are your options '
+    ) : React.createElement(
+        'p',
+        null,
+        ' no options'
+    )
 );
+
+var user = {
+    name: "jewel",
+    age: 30,
+    location: "new york"
+};
+
+function getLocation(loc) {
+    if (loc) {
+        return React.createElement(
+            'p',
+            null,
+            'location : ',
+            loc
+        );
+    }
+}
+
+function isAdult(age) {
+    if (age > 18) return React.createElement(
+        'p',
+        null,
+        'age: ',
+        age
+    );
+    return React.createElement(
+        'p',
+        null,
+        'age is below 18'
+    );
+}
+
+var templatetwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var approot = document.getElementById('app');
 ReactDOM.render(template, approot);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,44 @@
 
 // JSX
 
-var template = <h1>this is JSX from app.js saying hello</h1>;
+var data = {
+    title: 'indicision app',
+    subtitle: 'put your life',
+    options: ['one', 'two']
+}
+
+var template = (
+    <div>
+        <h1>{data.title}</h1>
+        {(data.subtitle && data.subtitle.length > 0) && <p>{data.subtitle}</p>}
+        {(data.options && data.options.length > 0) ? <p>here are your options </p> : <p> no options</p>}
+    </div>
+);
+
+var user = {
+    name: "jewel",
+    age: 30,
+    location: "new york"
+}
+
+function getLocation(loc) {
+    if (loc) {
+        return <p>location : {loc}</p>;
+    }
+}
+
+function isAdult(age) {
+    if (age > 18) return <p>age: {age}</p>
+    return <p>age is below 18</p>
+}
+
+var templatetwo = (
+    <div>
+        <h1>{user.name ? user.name : 'anonymous'}</h1>
+        { (user.age && user.age >= 18) && <p>age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
 var approot = document.getElementById('app')
 ReactDOM.render(template, approot);
