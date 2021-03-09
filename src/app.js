40d6c29e6238fbb6570601ee982f95491dc42624
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-09 21:42:24 +0530

    passing JSX to components

--- src/app.js
+++ src/app.js
@@ -3,4 +3,22 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+// passing JSX to component
+const Layout = (props) => {
+    return (
+        <div>
+            <h1>Header</h1>
+            {props.children}
+            <p>footer</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Layout>
+        <div>
+            <p>hello from children</p>
+        </div>
+    </Layout>,
+    document.getElementById('app'))
+// ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
