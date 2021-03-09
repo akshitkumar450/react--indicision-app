c3f6cab59b086709e414de8f6d4d15d893c9448e
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-09 18:20:06 +0530

    using babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('app.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const temp = <p>hello</p>
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hello from babel with react</h1>
+                <p>lorem20  </p>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Header />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,21 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 }
+
+
